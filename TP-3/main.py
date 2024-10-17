f30a0f6e3580d0f5f7935a4b08f2a9eef229c833
LautaCarron <166958031+LautaCarron@users.noreply.github.com>
2024-10-17 10:46:11 -0300

    unos cambios

--- TP-3/main.py
+++ TP-3/main.py
@@ -6,18 +6,6 @@ def abrir_archivo(nombre_archivo):
     # Abriendo un archivo de imagen en modo binario
     with open(nombre_archivo, "rb") as archivo:
         contenido_binario = archivo.read()  # Lee todo el contenido del archivo como bytes
-
-    #print(contenido_binario)  # Imprime los bytes, puedes procesarlos o guardarlos en un arreglo
-
-    #Contar la frecuencia de cada byte
-    conteo_bytes = Counter(contenido_binario)
-
-    #Separar los bytes únicos y sus frecuencias en dos listas
-    bytes_unicos = list(conteo_bytes.keys())
-    frecuencia_bytes = list(conteo_bytes.values())
-
-    #print("Bytes únicos:", bytes_unicos)
-    #print("Frecuencia de bytes:", frecuencia_bytes)
     return contenido_binario
 
 def obtener_probabilidades(contenido):
@@ -92,7 +80,7 @@ directorio_actual = os.path.dirname(os.path.abspath(__file__))
 
 # Combinar la ruta de la carpeta con el nombre del archivo
 #ruta_completa = os.path.join(directorio_actual, "prueba.txt")
-ruta_completa = os.path.join(directorio_actual, "facultad.png")
+ruta_completa = os.path.join(directorio_actual, "prueba.txt")
 # Leemos el archivo de entrada y lo almacenamos
 contenido_binario = abrir_archivo(ruta_completa)
 # Calculamos la distribución de probabilidades para cada símbolo
